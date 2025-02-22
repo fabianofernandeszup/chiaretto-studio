f816e812219f1f7decaef404e32924a5b952bd3c
fabiano.fernandes@stg.stackspot.com <fabiano.fernandes@stg.stackspot.com>
2025-02-22 11:27:49 -0300

    Update action approve execution
    Signed-off-by: fabiano.fernandes@stg.stackspot.com <fabiano.fernandes@stg.stackspot.com>

--- actions/action-v3-approve-execution-workflow/script.py
+++ actions/action-v3-approve-execution-workflow/script.py
@@ -5,7 +5,7 @@ from oscli import __workflow_base_url__
 
 def run(metadata):
     result = post_with_authorization(
-        url=f"{__workflow_base_url__}/v1/executions/{metadata.inputs['var']['STK_WORKFLOW_EXECUTION_ID']}/jobs/{metadata.inputs['job_id']}/accept",
+        url=f"{__workflow_base_url__}/v1/executions/{metadata.inputs['execution_id']}/jobs/{metadata.inputs['job_id']}/accept",
     )
     if not result.ok:
         print(result.text)
